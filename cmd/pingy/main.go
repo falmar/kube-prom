808bbe2bda86ad2f9e8207287d64046da9066dbc
David Lavieri <daviddlavier@gmail.com>
2019-02-27 22:21:06 -0400

    Several changes
    - Change namespace to default
    - Add pingy go app build/deployment configs
    - Add ClusterIP service for requesty

--- cmd/pingy/main.go
+++ cmd/pingy/main.go
@@ -3,39 +3,47 @@ package main
 import (
 	"log"
 	"net/http"
+	"net/url"
+	"os"
 	"time"
 )
 
 func main() {
-	addr := "http://192.168.99.100:31212"
+	addr := os.Getenv("REQUESTY_HOST")
 
-	for i := 0; i < 30; i++ {
-		go func() {
-			for {
-				_, err := http.Get(addr + "/handler")
-
-				if err != nil {
-					log.Println("handler err:", err)
-					return
-				}
-
-				<-time.After(time.Millisecond * 100)
-			}
-		}()
-
-		go func() {
-			for {
-				_, err := http.Get(addr + "/error")
-
-				if err != nil {
-					log.Println("handler err:", err)
-					return
-				}
-
-				<-time.After(time.Millisecond * 500)
-			}
-		}()
+	if addr == "" {
+		// minikube
+		addr = "http://192.168.99.100:31212"
 	}
 
-	time.Sleep(time.Minute * 5)
+	foo := func() error {
+		req := &http.Request{
+			URL: &url.URL{
+				Host: addr,
+				Path: "/handler",
+				Scheme: "http",
+			},
+		}
+		clt := &http.Client{}
+
+		_, err := clt.Do(req)
+
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	for {
+		err := foo()
+
+		if err != nil {
+			log.Println("handler err:", err)
+
+			return
+		}
+
+		<-time.After(time.Millisecond * 100)
+	}
 }
